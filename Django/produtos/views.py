27480c5cd8d0cc07f59efc03c1728ca318f0ae25
Guilhermo <guilhermo4t2l@gmail.com>
2024-12-05 07:51:30 -0300

    django v2

--- Django/produtos/views.py
+++ Django/produtos/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def ver_produto(request):
+ if request.method =="GET":
     return render(request, 'ver_produto.html', {'nome':'caio'})
+ elif request.method == "POST":
+    return HttpResponse('MUITO FODA')
 def inserir_produto(request):
     return HttpResponse("produto")
